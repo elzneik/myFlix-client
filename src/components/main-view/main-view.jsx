4e16e1f6bd0c5ddbf2c81923469e2664f5702f71
elzneik <104824463+elzneik@users.noreply.github.com>
2022-08-01 16:46:43 +0200

    update folder

---
+++ src/components/main-view/main-view.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export class MainView extends React.Component{
+    render(){
+        return(
+                <div className="MainView">
+                    <div>Inception</div>
+                    <div>The Shawshank Redemption</div>
+                    <div>Gladiator</div>
+                </div>
+        );
+    }
+}

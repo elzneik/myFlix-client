0a49950b09aa7e7fe9d23543d9d84e7c23c3b783
elzneik <104824463+elzneik@users.noreply.github.com>
2022-08-03 19:26:58 +0200

    update main-view syntax

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -11,19 +11,11 @@ export class MainView extends React.Component{ //creates MainView Component
             { _id: 1, Title: 'Inception', Description: 'desc1...', ImagePath: '...'},
             { _id: 2, Title: 'The Shawshank Redemption', Description: 'desc2...', ImagePath: '...'},
             { _id: 3, Title: 'Gladiator', Description: 'desc3...', ImagePath: '...'}
-          ]
-        }
-      }
-
-export class MainView extends React.Component{
-        constructor() {
-          super();
-          this.state = {
-            movies: [...],
+          ],
             selectedMovie: null
           };
         }
-      }
+      
 
       
     render() {

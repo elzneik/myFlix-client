0adf7ee264dc867fbfa0cb295a6f0c8f7bb3d421
elzneik <104824463+elzneik@users.noreply.github.com>
2022-08-15 14:34:11 +0200

    update changes

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -4,7 +4,6 @@ export class MovieCard extends React.Component {
 
 render() {
     const { movie, onMovieClick } = this.props;
-
     return <div className="movie-card" onClick={() => { onMovieClick(movie); }}>{movie.Title}</div>;
   }
 }

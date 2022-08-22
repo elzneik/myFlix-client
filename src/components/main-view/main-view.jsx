9344b0446ee150a0c0f163d488a2457a40cc089c
elzneik <104824463+elzneik@users.noreply.github.com>
2022-08-22 10:47:47 +0200

    update

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -8,6 +8,7 @@ import { LoginView } from '../login-view/login-view';
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from '../movie-view/movie-view';
 import { RegistrationView } from '../registration-view/registration-view';
+import { navBar } from '../movie-view/movie-view';
 
 
 export class MainView extends React.Component{ //creates MainView Component 

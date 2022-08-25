08cc015c777afe280b08b299d15ce5b7dbd57b75
elzneik <104824463+elzneik@users.noreply.github.com>
2022-08-25 13:53:18 +0200

    update Nav-view

--- src/components/nav-view/nav-view.jsx
+++ src/components/nav-view/nav-view.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import { Navbar, Container, Nav, Button } from "react-bootstrap"; 
+import { Link } from "react-router-dom";
+
+import "./navbar.scss";
 
 export function NavBar() {
+  let user = localStorage.getItem("user");
 // method to sign user out
-const onLoggedOut = () => {
+
+const handleLogOut = (e) => {
+  e.preventDefault();
   localStorage.clear();
   window.open("/", "_self");
-}
+  props.onLoggedOut(user);
+};
 
 // method to return a token from local storage
 const isAuth = () => {
@@ -24,15 +31,19 @@ const isAuth = () => {
 return (
   <Navbar className='main-nav' bg="light" expand="lg" variant="light" sticky="top">
     <Container>
-      <Navbar.Brand className="navbar-logo" href="#home">MyFlixMovies</Navbar.Brand>
+      <Navbar.Brand className="navbar-logo" href="/">MyFlixMovies</Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="ml-auto">
+        <Nav className="me-auto">
           {isAuth() && ( 
-            <Nav.Link href={`/user/${user}`}>{User}</Nav.Link>
+            <Nav.Link as={Link} to={`/user/${user}`}>
+              {User}
+            </Nav.Link>
           )}
           {isAuth() && ( 
-            <button variant="link" onClick={() => {this.onLoggedOut()}}>LogOut</button> 
+            <button variant="link" onClick={handleLogOut}>
+              Logout
+            </button> 
           )}
           {!isAuth() && ( 
             <Nav.Link href="/">Sign-in</Nav.Link>

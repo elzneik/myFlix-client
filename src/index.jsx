ca477e3e6953f3b8545856f9dbab90f0faf4bb9a
elzneik <104824463+elzneik@users.noreply.github.com>
2022-09-22 21:53:48 +0200

    update redux toolkit

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Container from 'react-bootstrap/Container';
-import { createStore, configureStore } from 'redux';
+import { configureStore } from 'redux';
 import { Provider } from 'react-redux';
 import moviesApp from './reducers/reducers';
 import { devToolsEnhancer } from 'redux-devtools-extension';

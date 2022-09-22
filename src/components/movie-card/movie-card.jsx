9d33f2ec9bf3dfad2ebf96b0f117eba0e24613f8
elzneik <104824463+elzneik@users.noreply.github.com>
2022-09-22 22:14:07 +0200

    update movie-card

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -38,6 +38,7 @@ render() {
   }
 }
 
+/*
 MovieCard.propTypes = {
   movie: PropTypes.shape({
     Title: PropTypes.string.isRequired,
@@ -55,9 +56,9 @@ MovieCard.propTypes = {
   }).isRequired,
   onMovieClick: PropTypes.func.isRequired,
 };
+*/
 
 
-/*
 // set up for propType to validate data
 MovieCard.propTypes = {
   movie: PropTypes.shape({
@@ -67,7 +68,7 @@ MovieCard.propTypes = {
   }).isRequired,
   onMovieClick: PropTypes.func,
 };
-*/
+
 
 /*
 MovieCard.propTypes = {
